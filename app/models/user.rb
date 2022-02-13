1bb0ce4e6115b298dd6d307caa5000e59a0d822f
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-02-13 15:01:15 +0900

    [add]addoress_valide_unique_index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true,  length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  #正規表現で記号のマッチングを行う
+  validates :email, presence: true, length: { maximum: 255},format: { with: VALID_EMAIL_REGEX },
+                                                            uniqueness: { case_sensitive: false }
+  #メールアドレスの大文字小文字は無視した検証を行なっているためcase_sensitive: false
+
 end
