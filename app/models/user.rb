114bfa7ee7eb8da0288fee85b0da75dd911f3478
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-02-13 15:31:07 +0900

    [add]UserPassword
    [add]UserPasswordDigest_addgem_bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = self.email.downcase }#保存を行う前にdowncaseしたアドレスをメソッドに代入している
   validates :name, presence: true,  length: { maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  #正規表現で記号のマッチングを行う
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i#正規表現で記号のマッチングを行う
   validates :email, presence: true, length: { maximum: 255},format: { with: VALID_EMAIL_REGEX },
                                                             uniqueness: { case_sensitive: false }
   #メールアドレスの大文字小文字は無視した検証を行なっているためcase_sensitive: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220213055733) do
+ActiveRecord::Schema.define(version: 20220213062533) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "passoword_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
