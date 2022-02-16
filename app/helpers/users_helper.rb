42d8dbffaaa53b328d135d9ad8119d64e69a21e7
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-02-16 22:05:08 +0900

    [add]UserSignUpForm

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
 
-  def gravatar_for(user)
+  def gravatar_for(user, size: 50)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)#digestメソッドでemailをMD5ハッシュ
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"#ハッシュ化されたidをgravatarに紐つける
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"#ハッシュ化されたidをgravatarに紐つける
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
