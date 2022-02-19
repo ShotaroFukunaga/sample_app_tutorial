611b0458542ec1e1a035709aeea2f6adf195215b
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-02-19 19:40:46 +0900

    [add]integration/users_signup_in_valid_signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         assert_select 'li',"Password is too short (minimum is 6 characters)"
       end
     end
+
+    test "valid signup infomation" do
+      get signup_path
+      assert_difference 'User.count', 1 do
+        post users_path, params:{ user: { name: "Example user",
+                                          email: "user@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password"} }
+        end
+        follow_redirect!
+        assert_template 'users/show'
+      end
 end
