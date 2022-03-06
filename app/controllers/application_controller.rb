e9e454ab120b9c01e20c27a170c915bd135c639e
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-03-06 18:20:35 +0900

    [create]micropost_create_view_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello world!!"
+  private
+
+  def logged_in_user
+    unless logged_in?
+      store_location#getリクエストなら記憶しておく
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
   end
+
 end
